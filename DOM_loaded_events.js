e198786fcd4e0bf767ccded274faa01bfcce8db1
Saranya <jsaran.bj@gmail.com>
2025-03-14 12:07:32 +0100

    Async and defer

--- DOM_loaded_events.js
+++ DOM_loaded_events.js
@@ -1,1 +1,30 @@
-// How  DOM works. Parse all the html tag into object and emit an event called DOMContentLoaded
+// How  DOM works. Parse all the html tag into object and emit an event called DOM
+
+
+document.addEventListener('DOMContentLoaded',() => {
+    const myApp = document.getElementById("app")
+    const heading = myApp.children[0];
+    const image = myApp.children[1];
+
+    //myApp.innerHTML="<h1> Change title </h1>"
+    heading.innerText = 'i have changed'
+    console.log(`${image.offsetWidth} , ${image.offsetHeight}`)
+})
+
+window.addEventListener('load' , () => {
+    const myApp = document.getElementById("app")
+    const heading = myApp.children[0];
+    const image = myApp.children[1];
+
+    //myApp.innerHTML="<h1> Change title </h1>"
+    heading.innerText = 'i have changed'
+    console.log(`${image.offsetWidth} , ${image.offsetHeight}`)
+})
+
+
+window.addEventListener('unload',() => {
+
+})
+
+
+window.onbeforeunload
